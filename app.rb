7c3c23231566cea5ab4f82b995e43b8b085e9455
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 19:17:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,12 +12,12 @@ class App < Sinatra::Base
 
     get '/hometown' do
       "My hometown is "
-      resp.status = 200
+    
     end
 
     get '/favorite-song' do
       "My favorite song is  "
-      resp.status = 200
+
     end
 
 end
